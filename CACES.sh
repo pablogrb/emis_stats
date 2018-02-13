592b4078bdd2fca50b60a99881986edee69e3e12
Pablo Garcia <pablogar@barney.ce.cmu.edu>
2018-02-13 16:32:23 -0500

    Automation start new file:   CACES.sh new file:   run_emis_stats.sh * Based on the scripts and templates for combine_point_emis

---
+++ CACES.sh
@@ -0,0 +1,5 @@
+BASE_PATH=/Users2/pablogar/CACES_2017/12US2/pnt
+DATE_PATH=/Users2/pablogar/NEI11_platform/smoke3.7/scripts/smk_dates/2017
+no_sectors=5
+sector_names=(othpt   pt_oilgas ptegu ptfire ptnonipm)
+sector_dtype=(mwdss_N mwdss_N   all   all    mwdss_Y)
