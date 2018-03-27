361d78dd1f4ef88481f0a33dbbe93ab36a053419
Pablo Garcia <pablogar@barney.ce.cmu.edu>
2018-03-26 21:54:12 -0400

    Main code for totalizer modified:   .gitignore * Added exceptions for the new exec and the binaries new file:   camxproj.txt * Needed for VERDI plotting modified:   compile.sh * Added line for the totalizer new file:   totalize_othar.in * Scenario file for othar in the totalizer * Contains the number of files and their paths new file:   totalize_uam.f90 * Calculates the totals using the utils module * Declarations and variable dictionary * Command line argument capture for control file * Namelist reading * Opens the files and then calls concantenate, totalize and write new file:   totalize_uam.in * same as the othar file

--- compile.sh
+++ compile.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 rm class_uam_iv.mod
+rm utils_uam_iv.mod
 rm csv_file.mod
 rm emis_stats
+rm totalize_uam
 
-ifort class_uam_iv.f90 csv_file.f90 emis_stats.f90 -o emis_stats -g -mieee-fp -align dcommons -convert big_endian -static_intel -extend-source
+ifort class_uam_iv.f90 csv_file.f90     emis_stats.f90   -o emis_stats   -g -mieee-fp -align dcommons -convert big_endian -static_intel -extend-source
+ifort class_uam_iv.f90 utils_uam_iv.f90 totalize_uam.f90 -o totalize_uam -g -mieee-fp -align dcommons -convert big_endian -static_intel -extend-source
---
+++ totalize_uam.f90
@@ -0,0 +1,100 @@
+! ------------------------------------------------------------------------------------------
+! Main Program
+! ------------------------------------------------------------------------------------------
+
+PROGRAM totalize_uam
+
+USE class_uam_iv
+USE utils_uam_iv
+
+IMPLICIT NONE
+
+! ------------------------------------------------------------------------------------------
+! Purpose:
+! 	Calculates totals for all pollutants of type
+!	'EMISSIONS '
+!	This is a limitation of the utils_uam_iv implementation, for now
+! Inputs:
+! 	Control file (namelist)
+! Outputs:
+!	UAM-IV file with one data frame of the total for the period
+! By:
+!	Pablo Garcia
+!	03-2018
+! NOTE:
+!
+
+! ------------------------------------------------------------------------------------------
+! Declarations
+TYPE(UAM_IV), ALLOCATABLE :: fl_inp(:)	! Input UAM-IV files
+TYPE(UAM_IV) :: fl_int					! intermediate UAM-IV file
+TYPE(UAM_IV) :: fl_out
+CHARACTER(LEN=256), ALLOCATABLE :: inp_file(:)
+INTEGER :: nfiles							! Number of input files
+CHARACTER(LEN=256) :: out_file				! File names
+
+! Argument control
+INTEGER :: arg_num
+CHARACTER(LEN=2) :: arg_switch
+! LOGICAL :: file_exists
+
+! Namelist IO
+CHARACTER(LEN=256) :: ctrlfile					! Control file path
+INTEGER :: nml_unit
+NAMELIST /FILE_CONTROL/ nfiles
+NAMELIST /FILE_IO/ inp_file, out_file
+
+! Counters
+INTEGER :: i_fl
+
+! ------------------------------------------------------------------------------------------
+! Entry point
+! ------------------------------------------------------------------------------------------
+!
+! Command line argument capture
+arg_num = COMMAND_ARGUMENT_COUNT()
+IF (arg_num .EQ. 0) THEN
+	ctrlfile = 'totalize_uam.in'
+	WRITE(*,*) 'Using the control file ', TRIM(ctrlfile)
+ELSEIF (arg_num .NE. 2) THEN
+	WRITE(*,*) 'Bad argument number'
+	CALL EXIT(0)
+ELSE
+	! Capture the argument type
+	CALL GET_COMMAND_ARGUMENT(1,arg_switch)
+	IF (arg_switch .NE. '-f') THEN
+		WRITE(*,*) 'Bad argument type'
+		CALL EXIT(0)
+	ELSE
+		CALL GET_COMMAND_ARGUMENT(2,ctrlfile)
+		WRITE(*,*) 'Using the control file ', TRIM(ctrlfile)
+	END IF
+END IF
+
+! Read the namelist
+OPEN(NEWUNIT=nml_unit, FILE=ctrlfile, FORM='FORMATTED', STATUS='OLD', ACTION='READ')
+! Get the number of files
+READ(nml_unit,NML=FILE_CONTROL)
+! Allocate
+ALLOCATE(inp_file(nfiles))
+ALLOCATE(fl_inp(nfiles))
+! Get the paths
+READ(nml_unit,NML=FILE_IO)
+CLOSE(nml_unit)
+
+! Open the files
+DO i_fl = 1, nfiles
+	! Open each file
+	CALL read_uamfile(fl_inp(i_fl),inp_file(i_fl))
+END DO
+
+! Concatenate
+CALL concatenate(fl_inp,fl_int)
+
+! Totalize
+CALL totalize(fl_int,fl_out)
+
+! Write out
+CALL write_uamfile(fl_out,out_file)
+
+END PROGRAM totalize_uam
